590fa59f4d09d5ac4d0ce1b67178fb8f9d564de7
Lovnyth Valcin <lovnyth@gmail.com>
2025-02-04 01:21:50 +0000

    p tag added in html

--- src/app.js
+++ src/app.js
@@ -7,7 +7,9 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   let pronoun =['the','our','their','his', 'her']
-  let adj =['great','big','amazing','astonishing','huge']
-  let noun = ['jogger','horse','dog','house','car']
+  let adj =['great','big','amazing','fancy','huge']
+  let noun = ['recipe','horse','dog','house','car']
   console.log("Hello Rigo from the console!");
 };
+
+domainName=
