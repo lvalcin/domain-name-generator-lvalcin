5fd8c586393523e7de6e7ffa8359ee11c7fc3d5d
Lovnyth Valcin <lovnyth@gmail.com>
2025-02-04 01:16:06 +0000

    added 3 arrays to js file

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronoun =['the','our','their','his', 'her']
+  let adj =['great','big','amazing','astonishing','huge']
+  let noun = ['jogger','horse','dog','house','car']
   console.log("Hello Rigo from the console!");
 };
