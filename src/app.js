2579b5e5c591c96a3481643f5c237f133415f43a
Lovnyth Valcin <lovnyth@gmail.com>
2025-02-08 05:53:09 +0000

    for function added to code

--- src/app.js
+++ src/app.js
@@ -5,11 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+// console.log("Any string");
 window.onload = function() {
-  let pronoun =['the','our','their','his', 'her']
-  let adj =['great','big','amazing','fancy','huge']
-  let noun = ['recipe','horse','dog','house','car']
-  console.log("Hello Rigo from the console!");
-};
-
-domainName=
+  let pronoun =['the','our','his', 'her'];
+  let adjective =['great','big','amazing','fancy'];
+  let noun = ['recipe','horse','dog','car'];
+  let extension = ['.com', '.net', '.us', '.io'];
+  
+  let domainName = [...pronoun,...adjective,...noun];
+  
+  for(let i=0;i<pronoun.length;i++){
+    for(let a=0; a<adjective.length;a++){
+      for(let n=0; b<noun.length; n++){
+        for(let e=0; 0<extension.length; e++){
+          
+          let domainName = pronoun[i] = adjective[a] + noun[n] + extension[e];
+        }
+      }
+    }
+    
+  }
+}
+console.log(domainName);
